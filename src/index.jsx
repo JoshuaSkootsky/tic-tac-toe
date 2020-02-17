24ac5f9ff8b1f24f7657bdfe61c1fead77f5df1f
JoshuaSkootsky <joshua.skootsky@gmail.com>
2020-02-16 23:10:39 -0500

    game is working

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { calculateWinner } from './utils';
 
 //component number one.
 
@@ -18,13 +19,16 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(' '),
+      squares: Array(9).fill(null),
       XisNext: true,
     };
   }
 
   handleClick(i) {
     const squares = [...this.state.squares]; // copy
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.XisNext ? 'X' : 'O';
     this.setState({ squares, XisNext: !this.state.XisNext });
   }
@@ -39,8 +43,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      console.log('Setting status...', this.state.XisNext);
+      status = 'Next player: ' + (this.state.XisNext ? 'X' : 'O');
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -80,6 +90,26 @@ class Game extends React.Component {
     );
   }
 }
+
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i];
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a];
+//     }
+//   }
+//   return null;
+// }
 
 // ========================================
 
