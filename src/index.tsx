8f901dd23a00006299b1cc570a7ec0c1b0150246
JoshuaSkootsky <joshua.skootsky@gmail.com>
2020-02-16 21:04:22 -0500

    trying to make the .tsx work

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { calculateWinner } from './utils';
 
 //component number one.
 
 // Replace this.state.value with this.props.value in Square’s render method
 // Replace this.setState() with this.props.onClick() in Square’s render method
 // Delete the constructor from Square because Square no longer keeps track of the game’s state
-const Square = props => (
-  <button className="square" onClick={props.onClick}>
-    {props.value}
+const Square = ({ value, onClick }) => (
+  <button className="square" onClick={onClick}>
+    {value}
   </button>
 );
 
@@ -19,13 +20,20 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(' '),
+      XisNext: true,
     };
   }
 
   handleClick(i) {
-    const squares = [...this.state.squares]; // copy
-    squares[i] = 'X';
-    this.setState({ squares });
+    const squares = [...this.state.squares];
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    squares[i] = this.state.XisNext ? 'X' : 'O'; // immutable, change a copy
+    this.setState({
+      squares,
+      XisNext: !this.state.XisNext,
+    });
   }
 
   renderSquare(i) {
@@ -38,7 +46,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
