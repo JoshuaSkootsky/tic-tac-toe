afc4bc6f351a3dfe8737722bbd649ac293cd1e96
JoshuaSkootsky <joshua.skootsky@gmail.com>
2020-02-26 21:07:45 -0500

    just kidding about no semicolons

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { calculateWinner } from './utils'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { calculateWinner } from './utils';
 
 //component number one - an individual square
 const Square = ({ onClick, value }) => (
   <button className="square" onClick={onClick}>
     {value}
   </button>
-)
+);
 
 // component #2
-const Board = props => {
+const Board = (props) => {
   // render square utility function
-  const renderSquare = i => {
-    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />
-  }
+  const renderSquare = (i) => {
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
+  };
 
   return (
     <div>
@@ -35,69 +35,69 @@ const Board = props => {
         {renderSquare(8)}
       </div>
     </div>
-  )
-}
+  );
+};
 
 // component #3 - Game
 const Game = () => {
   // instead of constructor, initialize state hooks for history, state, and XisNext
-  const [stepNumber, setStepNumber] = useState(0)
-  const [history, setHistory] = useState([{ squares: [] }])
-  const [XisNext, setXisNext] = useState(true)
+  const [stepNumber, setStepNumber] = useState(0);
+  const [history, setHistory] = useState([{ squares: [] }]);
+  const [XisNext, setXisNext] = useState(true);
 
-  const handleClick = i => {
-    setHistory(history.slice(0, stepNumber + 1))
+  const handleClick = (i) => {
+    setHistory(history.slice(0, stepNumber + 1));
 
-    const current = history[history.length - 1] // get current squares
-    const squares = [...current.squares] // make a safe copy
+    const current = history[history.length - 1]; // get current squares
+    const squares = [...current.squares]; // make a safe copy
     if (calculateWinner(squares) || squares[i]) {
-      return // do nothing on this click
+      return; // do nothing on this click
     }
     // update squares copy with current move
-    squares[i] = XisNext ? 'X' : 'O'
+    squares[i] = XisNext ? 'X' : 'O';
     // update history
-    setHistory(history.concat([{ squares }]))
-    setStepNumber(history.length)
-    setXisNext(!XisNext)
-  }
+    setHistory(history.concat([{ squares }]));
+    setStepNumber(history.length);
+    setXisNext(!XisNext);
+  };
 
-  const jumpTo = step => {
-    setStepNumber(step)
-    setXisNext(step % 2 === 0)
-  }
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0);
+  };
   // this move list will be displayed later
   const moves = history.map((e, i) => {
-    const desc = i ? 'Go to move #' + i : 'Go to game start'
+    const desc = i ? 'Go to move #' + i : 'Go to game start';
     return (
       <li key={i}>
         <button onClick={() => jumpTo(i)}>{desc}</button>
       </li>
-    )
-  })
+    );
+  });
 
-  const current = history[stepNumber]
-  const winner = calculateWinner(current.squares)
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
 
-  let status
+  let status;
   if (winner) {
-    status = 'Winner: ' + winner
+    status = 'Winner: ' + winner;
   } else {
-    status = 'Next player: ' + (XisNext ? 'X' : 'O')
+    status = 'Next player: ' + (XisNext ? 'X' : 'O');
   }
   // display
   return (
     <div className="game">
       <div className="game-board">
-        <Board squares={current.squares} onClick={i => handleClick(i)} />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
         <div>{status}</div>
         <ol>{moves}</ol>
       </div>
     </div>
-  )
-}
+  );
+};
 
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById('root'))
+ReactDOM.render(<Game />, document.getElementById('root'));
